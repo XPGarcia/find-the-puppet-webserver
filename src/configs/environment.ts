46419cc7463498d1f0b069240229ed05302fb94b
Xavier García <xavier.garcia@prometeo.dev>
2023-10-20 14:10:49 -0500

    Deleting old dependencies

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -4,7 +4,6 @@ import * as path from 'path';
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 export const environment = {
-  port: process.env.PORT ?? 3000,
-  certificatePath: process.env.SSL_CERT_PATH ?? 'src/certificate.crt',
-  keyPath: process.env.SSL_KEY_PATH ?? 'src/private.key'
+  env: process.env.APP_ENV ?? 'DEVELOPMENT',
+  port: process.env.PORT ?? 3000
 };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import { environment } from './configs';
 import app from './app';
-import * as https from 'https';
 import * as http from 'http';
+import * as https from 'https';
 import * as fs from 'fs';
 
-const serverOptions = {
-  cert: fs.readFileSync(environment.certificatePath),
-  key: fs.readFileSync(environment.keyPath)
-};
+// const serverOptions = {
+//   cert: fs.readFileSync(environment.certificatePath),
+//   key: fs.readFileSync(environment.keyPath)
+// };
 
-export const server = https.createServer(serverOptions, app);
+// export const server =
+//   environment.env === 'DEVELOPMENT'
+//     ? http.createServer(app)
+//     : https.createServer(serverOptions, app);
 
-// export const server = http.createServer(app);
+export const server = http.createServer(app);
 
 export const start = () => {
   // 8443
